55c684271ca2b02b4c34365aa90ec1c846947a3d
RicBM71 <rbartolome@sanaval.com>
2020-02-28 10:39:26 +0100

    arreglo ventas

--- app/Http/Controllers/Exportar/DetalleVentasController.php
+++ app/Http/Controllers/Exportar/DetalleVentasController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Exportar;
 use App\Tipo;
 use App\Clase;
 use App\Cobro;
+use App\Albalin;
+use App\Albaran;
 use Illuminate\Http\Request;
 use App\Rules\RangoFechaRule;
 use Illuminate\Support\Facades\DB;
@@ -47,6 +49,132 @@ class DetalleVentasController extends Controller
     }
 
     private function detalle($data){
+
+
+        if ($data['operacion'] == 'N')
+            $albaranes = $this->noFacturados($data);
+        elseif ($data['operacion'] == 'F')
+            $albaranes = $this->facturados($data);
+        else
+            $albaranes = $this->todos($data);
+
+        if ($albaranes == null)
+            return [];
+
+        $row_id = 0;
+        $alb_ant = -1;
+        $arr=array();
+        foreach ($albaranes as $row){
+
+            if ($alb_ant != $row->id){
+                $cobros = Cobro::selectRaw('fpago_id, SUM(importe) AS importe')->where('albaran_id', $row->id)->groupBy('fpago_id')->get();
+
+                $cobro_alb=array(0,0);
+                foreach ($cobros as $cobro){
+                    if ($cobro->fpago_id == 1)
+                        $cobro_alb[0] += $cobro->importe;
+                    else
+                        $cobro_alb[1] += $cobro->importe;
+                }
+                $alb_ant = $row->id;
+            }
+            else{
+                $cobro_alb=array(0,0);
+            }
+
+            //
+
+            $albaran = Albaran::find($row->id);
+
+            $albalins = Albalin::with('producto.clase')->where('albaran_id', $row->id)->get();
+
+            $primera = true;
+            foreach ($albalins as $albalin){
+
+                $row_id++;
+
+                if ($primera == false){
+                    $cobro_alb = array(0,0);
+                    $primera = false;
+                }
+
+                $arr[]=[
+                        'id'             => $row_id, // esto es para que no falle key de vue
+                        'albaran_id'     => $albaran->id,
+                        'albaran'        => $albaran->albaran,
+                        'fecha'          => $albaran->fecha_albaran,
+                        'serie_albaran'  => $albaran->serie_albaran,
+                        'referencia'     => $albalin->producto->referencia,
+                        'producto'       => $albalin->producto->nombre,
+                        'clase'          => $albalin->producto->clase->nombre,
+                        'precio_coste'   => $albalin->precio_coste,
+                        'importe_venta'  => $albalin->importe_venta,
+                        'margen'         => $albalin->importe_venta - $albalin->precio_coste,
+                        'efectivo'       => $cobro_alb[0],
+                        'banco'          => $cobro_alb[1],
+                ];
+            }
+
+        }
+
+        return $arr;
+
+    }
+
+    private function noFacturados($data){
+        return DB::table('albaranes')
+                    ->select(DB::raw(DB::getTablePrefix().'albaranes.id,'.
+                                    DB::getTablePrefix().'albaranes.albaran,'.
+                                    DB::getTablePrefix().'albaranes.iva_no_residente, MAX('.
+                                    DB::getTablePrefix().'cobros.fecha) AS fecha'))
+                        ->join('cobros', 'albaran_id', '=', 'albaranes.id')
+                        ->where('albaranes.empresa_id', session('empresa')->id)
+                        ->where('tipo_id', $data['tipo_id'])
+                        ->where('fase_id', '<>', 10)
+                        ->whereNull('factura')
+                        ->where('facturar', true)
+                        ->whereNull('albaranes.deleted_at')
+                        ->groupBy('albaranes.id','albaran','iva_no_residente')
+                        ->havingRaw('MAX('.DB::getTablePrefix().'cobros.fecha) >= ? AND MAX('.DB::getTablePrefix().'cobros.fecha) <= ?',[$data['fecha_d'],$data['fecha_h']])
+                        ->orderBy('fecha')
+                        ->get();
+    }
+
+    private function facturados($data){
+        return DB::table('albaranes')
+                    ->select(DB::raw(DB::getTablePrefix().'albaranes.id,'.
+                                    DB::getTablePrefix().'albaranes.albaran,'.
+                                    DB::getTablePrefix().'albaranes.iva_no_residente, fecha_factura AS fecha'))
+                        ->where('albaranes.empresa_id', session('empresa')->id)
+                        ->where('tipo_id', $data['tipo_id'])
+                        ->where('fase_id', '<>', 10)
+                        ->whereDate('fecha_factura', '>=', $data['fecha_d'])
+                        ->whereDate('fecha_factura', '<=', $data['fecha_h'])
+                        ->whereNull('albaranes.deleted_at')
+                        ->groupBy('albaranes.id','albaran','iva_no_residente')
+                        ->orderBy('fecha')
+                        ->get();
+    }
+
+    private function todos($data){
+        return DB::table('albaranes')
+                    ->select(DB::raw(DB::getTablePrefix().'albaranes.id,'.
+                                    DB::getTablePrefix().'albaranes.albaran,'.
+                                    DB::getTablePrefix().'albaranes.iva_no_residente, MAX('.
+                                    DB::getTablePrefix().'cobros.fecha) AS fecha'))
+                        ->join('cobros', 'albaran_id', '=', 'albaranes.id')
+                        ->where('albaranes.empresa_id', session('empresa')->id)
+                        ->where('tipo_id', $data['tipo_id'])
+                        ->where('fase_id', '<>', 10)
+                        ->where('facturar', true)
+                        ->whereNull('albaranes.deleted_at')
+                        ->groupBy('albaranes.id','albaran','iva_no_residente')
+                        ->havingRaw('MAX('.DB::getTablePrefix().'cobros.fecha) >= ? AND MAX('.DB::getTablePrefix().'cobros.fecha) <= ?',[$data['fecha_d'],$data['fecha_h']])
+                        ->orderBy('fecha')
+                        ->get();
+    }
+
+    private function detalle2($data){
 
         $campo_fecha = "fecha_albaran";
         if ($data['operacion'] == 'F'){
