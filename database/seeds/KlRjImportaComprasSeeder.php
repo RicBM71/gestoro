be176adb33f037ea0e18a4a87a3bd6bd713ce33e
RicBM71 <rbartolome@sanaval.com>
2020-03-04 15:38:32 +0100

    klrjimport

---
+++ database/seeds/KlRjImportaComprasSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class KlRjImportaComprasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+       $this->checkCliente(10000);
+
+       $compras = DB::connection('db2')->select('select * from compras WHERE empresa_id = ? AND fase_id()', [$id]);
+
+    }
+
+    private function checkCliente($id){
+
+         // leo de kilates, origen
+         $clientes = DB::connection('db2')->select('select * from klt_clientes WHERE id = ?', [$id]);
+
+         $creados = $existen = 0;
+         foreach ($clientes as $cliente_kil) {
+
+             $cliente = DB::table('clientes')->where('dni', $cliente_kil->dni);
+
+             if ($cliente->exists()){
+                 $existen++;
+                 $cliente_id = $cliente->first()->id;
+             }
+             else{
+                 $creados++;
+                 $cliente_id = $this->crearCliente($cliente_kil);
+             }
+
+
+         }
+
+        return $cliente_id;
+
+         \Log::info("Creados ".$creados);
+         \Log::info("Existe ".$existen);
+    }
+
+    private function crearCliente($cliente_kil){
+
+        $data = collect($cliente_kil);
+
+        $data = $data->toArray();
+        $data['id']=null;
+
+        \Log::info($data);
+
+        return DB::table('clientes')->insertGetId($data);
+
+    }
+}
