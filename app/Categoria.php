e2ad75e3a14fe0bceace0e8da017964521827c5c
RicBM71 <rbartolome@sanaval.com>
2020-10-02 10:15:53 +0200

    marcas y categorias

---
+++ app/Categoria.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    protected $fillable = [
+        'nombre', ,'username'
+    ];
+
+    public static function selCategorias()
+    {
+
+        return Categoria::select('id AS value', 'nombre AS text')
+            ->orderBy('nombre', 'asc')
+            ->get();
+
+    }
+}
