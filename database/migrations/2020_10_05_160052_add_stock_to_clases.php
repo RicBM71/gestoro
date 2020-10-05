f56e255e8d735c5f9f0d0c21e3a07486f29e2b9c
RicBM71 <rbartolome@sanaval.com>
2020-10-05 17:38:02 +0200

    arreglo migracion

--- database/migrations/2020_10_05_160052_add_stock_to_clases.php
+++ database/migrations/2020_10_05_160052_add_stock_to_clases.php
@@ -26,7 +26,7 @@ class AddStockToClases extends Migration
     public function down()
     {
         Schema::table('clases', function (Blueprint $table) {
-            //
+            $table->dropColumn('stockable');
         });
     }
 }
